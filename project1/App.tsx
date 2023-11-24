6cbdb864e61b5dbcb9abb59e09bc351d5a6a667e
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2023-11-25 03:10:29 +0500

    practice some styles in React native

--- project1/App.tsx
+++ project1/App.tsx
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import DocumentPicker from "react-native-document-picker";
 
 export default function App() {
+  const buttonClicked = async () => {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images],
+      });
+      console.log("res", res);
+    } catch (error) {
+      if (DocumentPicker.isCancel(error)) {
+        console.log("error:", error);
+      }
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, styles.horizontal]}>
+      <ActivityIndicator color={"red"} />
+      <Button title="Click Button" color={"red"} onPress={buttonClicked} />
+      <TouchableOpacity onPress={buttonClicked}>
+        <Text>Input</Text>
+      </TouchableOpacity>
+      <Text style={styles.text} onPress={() => Alert.alert("press")}>
+        CLICK BUTTON
+      </Text>
+      <TextInput
+        placeholder="Input Text"
+        style={styles.input}
+        placeholderTextColor={"red"}
+      />
     </View>
   );
 }
@@ -13,8 +47,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    backgroundColor: "red",
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    color: "white",
+    fontWeight: "600",
+    borderRadius: 2,
+  },
+  horizontal: {
+    gap: 20,
+    // flexDirection: "row",
+  },
+  input: {
+    borderWidth: 2,
+    width: 120,
+    backgroundColor: "white",
+    color: "blue",
+    padding: 10,
   },
 });
