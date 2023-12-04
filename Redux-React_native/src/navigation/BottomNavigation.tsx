47a7b28bbe75839c15a69df4da614e77cd9d64a4
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2023-12-04 18:58:06 +0500

    Implement navigation with redux toolkit

--- Redux-React_native/src/navigation/BottomNavigation.tsx
+++ Redux-React_native/src/navigation/BottomNavigation.tsx
@@ -1,7 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AddFood from "../screens/AddFood";
-import { NavigationContainer } from "@react-navigation/native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 import Chat from "../screens/Chat";
 import Food from "../screens/Food";
 import Setting from "../screens/Setting";
@@ -10,47 +9,65 @@ import Home from "../screens/Home";
 const Tab = createBottomTabNavigator();
 
 function BottomNavigation() {
-
   return (
-    <NavigationContainer>
-      <Tab.Navigator 
-       
-      >
-        <Tab.Screen name="Home" component={Home} options={{
-          tabBarIcon:({focused, color,size})=>{
-            let iconName= focused? 'home': 'home-outline'
-            
-            return <Ionicons name={iconName}   size={20} color={"#e88e20"}/>
-          }
-          
-        }} />
-        <Tab.Screen name="AddFood" component={AddFood}  options={{
-          tabBarIcon:({focused, color,size})=>{
-            let iconName:string= focused? 'add-circle': 'add-circle-outline'
-            return <Ionicons name={iconName} size={20} color={"#e88e20"}/>
-          }
-        }}/>
-        <Tab.Screen name="Chat" component={Chat} options={{
-          tabBarIcon:({focused, color,size})=>{
-            let iconName:string= focused? 'chatbubbles': 'chatbubbles-outline'
-            return <Ionicons name={iconName} size={20} color={"#e88e20"}/>
-          }
-        }}/>
-        <Tab.Screen name="Food" component={Food} options={{
-          tabBarIcon:({focused, color,size})=>{
-            let iconName:string= focused? 'fast-food': 'fast-food-outline'
-            return <Ionicons name={iconName} size={20} color={"#e88e20"}/>
-          }
-        }}/>
-        <Tab.Screen name="Setting" component={Setting} options={{
-          tabBarIcon:({focused, color,size})=>{
-            let iconName:string= focused? 'fast-food': 'fast-food-outline'
-            return <Ionicons name={iconName} size={20} color={"#e88e20"}/>
-          }
-        }}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = focused ? "home" : "home-outline";
+
+            return <Ionicons name={iconName} size={20} color={"#e88e20"} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="AddFood"
+        component={AddFood}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = focused
+              ? "add-circle"
+              : "add-circle-outline";
+            return <Ionicons name={iconName} size={20} color={"#e88e20"} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = focused
+              ? "chatbubbles"
+              : "chatbubbles-outline";
+            return <Ionicons name={iconName} size={20} color={"#e88e20"} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Food"
+        component={Food}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = focused ? "fast-food" : "fast-food-outline";
+            return <Ionicons name={iconName} size={20} color={"#e88e20"} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = focused ? "fast-food" : "fast-food-outline";
+            return <Ionicons name={iconName} size={20} color={"#e88e20"} />;
+          },
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
-export default BottomNavigation
+export default BottomNavigation;
